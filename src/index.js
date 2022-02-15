8c3a39332e03acf8ab274b82ab62c1f3a3bc913f
antipachita <nice.strogonov@inbox.ru>
2022-02-15 23:10:27 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strLength = str.length;
+    let bracketLen = bracketsConfig.length;
+    if (str.length % 2 > 0) {
+      return false;
+     }
+    let arrayStr=str.split('');
+     console.log(arrayStr)
+    for (let i=0;i<arrayStr.length;i++){
+     for (let n=0;n<bracketLen;n++){
+                if (arrayStr[i]===bracketsConfig[n][0] && arrayStr[i+1]===bracketsConfig[n][1]){
+                    arrayStr.splice(i,2);
+                  console.log(arrayStr)
+                    i =- 1;
+                    break;
+                }
+        }
+    }
+    if (arrayStr.length===0) {
+    return true;
+    } else  {
+      return false;
+    }
 }
+
